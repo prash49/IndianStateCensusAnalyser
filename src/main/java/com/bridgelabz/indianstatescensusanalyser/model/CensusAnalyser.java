b2d2b6b02d6d12d9dff56770756ebea15f6464aa
Prashanth <imprash4u@gmail.com>
2021-09-27 22:33:31 +0530

    Added: Analyser ro load indain states census information from csv

--- src/main/java/com/bridgelabz/indianstatescensusanalyser/model/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/indianstatescensusanalyser/model/CensusAnalyser.java
@@ -1,7 +1,36 @@
 package com.bridgelabz.indianstatescensusanalyser.model;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
 public class CensusAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Indian State Census Analyser");
+
+    public int loadIndiaCensusData(String csvFilePath) {
+        Reader reader = null;
+        try {
+            reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBean<IndiaCensusCSV> csvToBean = new CsvToBeanBuilder<IndiaCensusCSV>(reader)
+                    .withType(IndiaCensusCSV.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            // iterator doesn't consume memory
+            Iterator<IndiaCensusCSV> iterator = csvToBean.iterator();
+            int numOfEntries = 0;
+            while (iterator.hasNext()) {
+                numOfEntries++;
+                iterator.next();
+            }
+            return numOfEntries;
+        } catch (
+                IOException e) {
+            System.out.println(e);
+        }
+        return 0;
     }
 }
--- src/test/java/com/bridgelabz/indianstatescensusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/indianstatescensusanalyser/CensusAnalyserTest.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.indianstatescensusanalyser;
 
+
+import com.bridgelabz.indianstatescensusanalyser.model.CensusAnalyser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class CensusAnalyserTest {
+    private String INIDAN_CENSUS_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        int count = censusAnalyser.loadIndiaCensusData(INIDAN_CENSUS_CSV_FILE_PATH);
+        Assert.assertEquals(29,count);
+    }
 }
+
